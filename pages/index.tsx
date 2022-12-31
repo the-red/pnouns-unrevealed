5a8312d8edca7b0a61a9fc5b948312ff8421ebd4
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-12-31 12:34:38 +0900

    refactor: コントラクトから直接SVGを取得

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
+import Web3 from 'web3'
 
 export default function Home() {
   const [tokenId, setTokenId] = useState<number>(1)
+  const [svg, setSvg] = useState<string>()
   const inputEl = useRef<HTMLInputElement>(null)
+
+  const generateSVGDocument = async (tokenId: number) => {
+    const web3 = new Web3(`https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`)
+    const tokenURIABI = [
+      {
+        inputs: [{ internalType: 'uint256', name: '_assetId', type: 'uint256' }],
+        name: 'generateSVGDocument',
+        outputs: [{ internalType: 'string', name: 'document', type: 'string' }],
+        stateMutability: 'view' as const,
+        type: 'function' as const,
+      },
+    ]
+    const tokenContract = '0x2e5C0BD35995ea7e8903C55ba66f28270310498f'
+    const contract = new web3.eth.Contract(tokenURIABI, tokenContract)
+    const svgXml = await contract.methods.generateSVGDocument(tokenId).call()
+    setSvg(Buffer.from(svgXml).toString('base64'))
+  }
+
+  useEffect(() => {
+    generateSVGDocument(tokenId)
+  }, [])
 
   return (
     <>
@@ -26,6 +48,7 @@ export default function Home() {
               const value = Number(inputEl.current?.value)
               if (Number.isFinite(value)) {
                 setTokenId(value)
+                generateSVGDocument(value)
               }
             }}
           >
@@ -51,7 +74,7 @@ export default function Home() {
               {Number.isFinite(tokenId) && (
                 <a href={`/images/${tokenId}.svg`} target="_blank" rel="noreferrer">
                   <picture>
-                    <img src={`/images/${tokenId}.svg`} alt={`${tokenId}.svg`} />
+                    <img src={`data:image/svg+xml;base64,${svg}`} alt={`${tokenId}.svg`} />
                   </picture>
                 </a>
               )}
