f261e07eb288ed9c327091a343ff7937cfd4b91e
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-03-25 20:21:52 +0900

    feat: 広告追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,26 @@ import Head from 'next/head'
 import { useRef, useState, useEffect } from 'react'
 import { generateSVGDocument } from './api/contract'
 
+const Ads = () => (
+  <>
+    <a href="https://px.a8.net/svt/ejp?a8mat=3T4O7G+EF62LM+3OP8+HWPVL" rel="nofollow">
+      <picture>
+        <img
+          width="320"
+          height="50"
+          alt=""
+          src="https://www25.a8.net/svt/bgt?aid=230325388872&wid=004&eno=01&mid=s00000017198003008000&mc=1"
+        />
+      </picture>
+    </a>
+    <picture>
+      <img width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=3T4O7G+EF62LM+3OP8+HWPVL" alt="" />
+    </picture>
+  </>
+)
+
 export default function Home() {
-  const [tokenId, setTokenId] = useState<number>(1)
+  const [tokenId, setTokenId] = useState<number>(0)
   const [svg, setSvg] = useState<string>()
   const inputEl = useRef<HTMLInputElement>(null)
 
@@ -78,6 +96,9 @@ export default function Home() {
           </form>
         </div>
       </main>
+      <footer className="container text-center" style={{ paddingTop: '40px', paddingBottom: '20px' }}>
+        <Ads />
+      </footer>
     </>
   )
 }
